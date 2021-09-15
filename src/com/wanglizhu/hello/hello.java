c9f1d014cf5286c945dd9230f48e2b9b10d925ed
wanglizhu <280499781@qq.com>
2021-09-16 00:32:59 +0800

    0102

---
+++ src/com/wanglizhu/hello/hello.java
@@ -0,0 +1,8 @@
+package com.wanglizhu.hello;
+
+public class hello {
+public static void main(String[] args) {
+	System.out.println("01");
+	System.out.println("02");
+}
+}
---
+++ src/com/wanglizhu/hello/hello2.java
@@ -0,0 +1,7 @@
+package com.wanglizhu.hello;
+
+public class hello2 {
+public static void main(String[] args) {
+	System.out.println("0202");
+}
+}
