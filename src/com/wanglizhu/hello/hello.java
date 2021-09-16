18214a58928023cd3753c683992703823e07e23e
wanglizhu <280499781@qq.com>
2021-09-16 12:09:39 +0800

    Update hello.java

--- src/com/wanglizhu/hello/hello.java
+++ src/com/wanglizhu/hello/hello.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("github03");
 	System.out.println("github修改01");
 	System.out.println("github修改02");
+	System.out.println("github修改03");
 }
 }
